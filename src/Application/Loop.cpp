5b5a470f59d4bf5cc3d3f1829d046d64c2a4d078
nsviel <nathan.sanchizviel@pm.me>
2023-10-06 21:33:06 +0200

    ok_now

--- src/Application/Loop.cpp
+++ src/Application/Loop.cpp
@@ -27,31 +27,20 @@ Loop::~Loop(){}
 
 //Main function
 void Loop::main_loop(){
-    UI_main gui(window);
-
   Param param;
-  Render_node* render_node = engine->get_node_render();
-  VK_engine* vk_engine = render_node->get_vk_engine();
-  Camera* cameraManager = render_node->get_cameraManager();
-  GPU_gui* gpu_gui = vk_engine->get_gpu_gui();
   //---------------------------
-
-
 
   window->create_window(param.window_dim.x, param.window_dim.y, param.window_title);
 
-
-  //gui.init_gui();
   engine->init();
   ui_loop->init(engine);
-
 
   auto start_time = std::chrono::steady_clock::now();
   while(!glfwWindowShouldClose(window->get_window())){
     glfwPollEvents();
+
     ui_loop->loop(engine);
     engine->loop();
-    //gui.run_gui_main();
   }
 
   engine->exit();
